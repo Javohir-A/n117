3496f680c7754d50c3cca861f853bf0dd700ee01
javohir-a <abdusamatovjavohir@gmail.com>
2024-09-26 17:54:25 +0500

    problem1.py qoshildi

---
+++ lesson7/problem1.py
@@ -0,0 +1,9 @@
+'''
+Author: javohir-a abdusamatovjavohir@gmail.com
+Date: 2024-09-26 17:53:41
+LastEditors: javohir-a abdusamatovjavohir@gmail.com
+LastEditTime: 2024-09-26 17:53:53
+FilePath: /lesson7/problem1.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+print("problem1")
